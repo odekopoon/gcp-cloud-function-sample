c155a3c68a3cd5e7e4a17afe71ff5fd1d26c48b8
Koji Yusa <odekopoon@gmail.com>
2017-03-20 02:15:07 +0900

    add sample cloud function. hello.core.hello

--- hello/src/hello/core.cljs
+++ hello/src/hello/core.cljs
@@ -3,6 +3,20 @@
 
 (nodejs/enable-util-print!)
 
+; Responds to any HTTP request that can provide a "message" field in the body.
+;
+; @param {!Object} req Cloud Function request context.
+; @param {!Object} res Cloud Function response context.
+(defn ^:export hello [req res]
+  ;  Example input: {"message": "Hello!"}
+  (if (= req.body.message js/undefined)
+    ; This is an error case, as "message" is required.
+    (.send (.status res 400) "No message defined!")
+    ; Everything is okay.
+    (do
+      (.log js/console req.body.message)
+      (.send (.status res 200) (str "Success: " req.body.message)))))
+
 (defn -main []
   (println "Hello world!"))
 
